5035dd560a979ac8666f080477b69b8ea357e47b
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-05-12 18:39:53 +0600

    redeploy-git index file corruption fix

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://job-nebula.web.app"],
+  })
+);
+app.use(express.json());
+
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ctz3uz9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const jobCollection = client.db("JobNebulaDB").collection("jobCollection");
+
+    //find all the jobs
+    app.get("/jobs", async (req, res) => {
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
+
+    // find single job data
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    //find my job data
+    app.get("/myJobs/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      const query = { email: userEmail };
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //post a job
+    app.post("/jobs", async (req, res) => {
+      const job = req.body;
+      const result = await jobCollection.insertOne(job);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("job nebula is looking for a job");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
